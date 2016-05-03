b751464edaaf7ed29e1009a1a8fa081e38c1fd24
turb <turb@pisscake.com>
2016-05-03 01:15:13 -0700

    text login

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -35,4 +35,46 @@ class MainControllerTest < ActionController::TestCase
 		assert_response :success
 		assert_select 'h1', 'Join the club'
 	end
+
+	test "login with correct credentials should set user_id in session and redirect" do
+
+		post :login, :login => {
+			:name => 'root',
+			:password => 'root'
+		}
+
+		assert_response :redirect
+		assert_includes session, :user_id
+
+		#should have no errors (error messages stored in ul)
+		assert_select 'ul', false
+	end
+
+	test "login with incorrect credentials shows error and has no user_id session information" do
+
+		post :login, :login => {
+			:name => 'root',
+			:password => 'incorrect password'
+		}
+
+		assert_response :success, 'should show index with errors when incorrect credentials'
+		assert_not_includes session, :user_id, 'user id should not be in session on incorrect credentials'
+
+		assert_select 'ul' do
+			assert_select 'li', 'incorrect name or password'
+		end
+
+		post :login, :login => {
+
+			:name => 'nonexistent user',
+			:password => 'root'
+		}
+
+		assert_response :success, 'should show index with errors when incorrect credentials'
+		assert_not_includes session, :user_id, 'user id should not be in session on incorrect credentials'
+
+		assert_select 'ul' do
+			assert_select 'li', 'incorrect name or password'
+		end
+	end
 end
