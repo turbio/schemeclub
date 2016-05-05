0e5d000c1644c752a67efb3e2ee27f8119a06173
turb <turb@pisscake.com>
2016-05-05 12:22:25 -0700

    user validation api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,9 @@
 class ApiController < ApplicationController
-  def valid_name
-  end
+	def valid_name
+		render plain: User.where('lower(name) = ?', params[:name].downcase).first.nil?.to_s
+	end
 
-  def valid_credentials
-  end
+	def valid_credentials
+		render plain: User.authenticate(params[:name], params[:password]).nil?.to_s
+	end
 end
