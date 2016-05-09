7d580cc6079557c72718adc58a5b79595e721547
turb <turb@pisscake.com>
2016-05-08 21:23:39 -0700

    prevent recruit code creation abuse

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -3,5 +3,7 @@ class DashController < ApplicationController
 		return render 'index' if session[:user_id].nil?
 		@user = User.find(session[:user_id])
 		@codes = RecruitCode.owned_by(@user)
+
+		@allowed_to_create_codes = @codes.length < 3
 	end
 end
