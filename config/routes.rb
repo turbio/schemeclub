8a7826209255de5d6cec51466b6beaa6f8c95c95
turbio <turbio@masonclayton.com>
2016-08-01 00:11:57 -0700

    new welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 	post '/login' => 'main#login'
 	post '/signup' => 'main#signup'
 	get '/logout' => 'main#logout'
+	get '/welcome' => 'main#welcome'
 	post '/new_code' => 'dash#new_code'
 
 	get '/:id' => 'main#join'
