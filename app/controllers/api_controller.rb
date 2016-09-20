1574472d8258c6b0eb5e21a70e6b96a645a51cd9
turbio <masongclayton@gmail.com>
2016-09-19 22:21:39 -0700

    api tests

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,19 @@
 class ApiController < ApplicationController
 	def valid_name
-		render json: !User.where('lower(name) = ?', params[:name].downcase).first.nil?
+		if params[:name]
+			render(json:
+				!User.where('lower(name) = ?', params[:name].downcase).first.nil?)
+		else
+			render status: 400, nothing: true
+		end
 	end
 
 	def valid_credentials
-		render json: !User.authenticate(params[:name], params[:password]).nil?
+		if params[:name] && params[:password]
+			render(json:
+				!User.authenticate(params[:name], params[:password]).nil?)
+		else
+			render status: 400, nothing: true
+		end
 	end
 end
--- spec/models/recruit_code_spec.rb
+++ spec/models/recruit_code_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 require 'time'
 
 RSpec.describe RecruitCode, type: :model do
-	it 'should create a recruit code using generator' do
-		owner = User.create!(name: 'username', password: 'password')
-		code = RecruitCode.generate_new_code owner
-	end
+	describe 'create' do
+		it 'should create a recruit code using generator' do
+			owner = User.create!(name: 'username', password: 'password')
+			code = RecruitCode.generate_new_code owner
+		end
 
-	it 'should not create a recruit code without an owner' do
-		expect do
-			code = RecruitCode.create!()
-		end.to raise_error(
-			ActiveRecord::RecordInvalid,
-			/Code can't be blank, Owner can't be blank, Claimed is not.../)
-	end
+		it 'should not create a recruit code without an owner' do
+			expect do
+				code = RecruitCode.create!()
+			end.to raise_error(
+				ActiveRecord::RecordInvalid,
+				/Code can't be blank, Owner can't be blank, Claimed is not.../)
+		end
 
-	it 'should generate recruit codes with random code' do
-		owner = User.create!(name: 'owner', password: 'password')
+		it 'should generate recruit codes with random code' do
+			owner = User.create!(name: 'owner', password: 'password')
 
-		first = RecruitCode.generate_new_code owner
-		second = RecruitCode.generate_new_code owner
-		third = RecruitCode.generate_new_code owner
+			first = RecruitCode.generate_new_code owner
+			second = RecruitCode.generate_new_code owner
+			third = RecruitCode.generate_new_code owner
 
-		expect(first.code).not_to eq(second.code)
-		expect(second.code).not_to eq(third.code)
+			expect(first.code).not_to eq(second.code)
+			expect(second.code).not_to eq(third.code)
+		end
 	end
 
 	it 'should get all codes owned by a user' do
@@ -37,42 +39,44 @@ RSpec.describe RecruitCode, type: :model do
 		expect(RecruitCode.owned_by(owner).first).to eq(first)
 	end
 
-	it 'should get code time remaining' do
-		owner = User.create!(name: 'owner', password: 'password')
-		code = RecruitCode.generate_new_code owner
+	describe 'availability' do
+		it 'should get code time remaining' do
+			owner = User.create!(name: 'owner', password: 'password')
+			code = RecruitCode.generate_new_code owner
 
-		expect(code.remaining_seconds).to be_a(Float)
-		expect(code.remaining_seconds).to be > 0
-	end
+			expect(code.remaining_seconds).to be_a(Float)
+			expect(code.remaining_seconds).to be > 0
+		end
 
-	it 'should not be available after claimed' do
-		owner = User.create!(name: 'owner', password: 'password')
-		code = RecruitCode.generate_new_code owner
+		it 'should not be available after claimed' do
+			owner = User.create!(name: 'owner', password: 'password')
+			code = RecruitCode.generate_new_code owner
 
-		expect(code.expired?).to eq(false)
-		expect(code.available?).to eq(true)
-		expect(code.availability).to eq(nil)
+			expect(code.expired?).to eq(false)
+			expect(code.available?).to eq(true)
+			expect(code.availability).to eq(nil)
 
-		code.claimed = true
-		code.save
+			code.claimed = true
+			code.save
 
-		expect(code.available?).to eq(false)
-		expect(code.availability).to eq('recruit code claimed')
-	end
+			expect(code.available?).to eq(false)
+			expect(code.availability).to eq('recruit code claimed')
+		end
 
-	it 'should not be available after expired' do
-		owner = User.create!(name: 'owner', password: 'password')
-		code = RecruitCode.generate_new_code owner
+		it 'should not be available after expired' do
+			owner = User.create!(name: 'owner', password: 'password')
+			code = RecruitCode.generate_new_code owner
 
-		expect(code.expired?).to eq(false)
-		expect(code.available?).to eq(true)
-		expect(code.availability).to eq(nil)
+			expect(code.expired?).to eq(false)
+			expect(code.available?).to eq(true)
+			expect(code.availability).to eq(nil)
 
-		#a while ago... Probably
-		code.created_at = Time.parse '1900-1-1'
-		code.save
+			#a while ago... Probably
+			code.created_at = Time.parse '1900-1-1'
+			code.save
 
-		expect(code.available?).to eq(false)
-		expect(code.availability).to eq('recruit code expired')
+			expect(code.available?).to eq(false)
+			expect(code.availability).to eq('recruit code expired')
+		end
 	end
 end
