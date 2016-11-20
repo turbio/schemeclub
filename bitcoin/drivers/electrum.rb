ca16264b8b4fe44e053d76c50ddc3aaf711135d8
turbio <masongclayton@gmail.com>
2016-11-19 23:43:10 -0800

    gcm fix/refactor some of electrum

--- bitcoin/drivers/electrum.rb
+++ bitcoin/drivers/electrum.rb
@@ -7,7 +7,7 @@ class Electrum
   def initialize(config)
     @config = config
     @cached_transactions = {}
-    @cached_address_transactions = Hash.new do |h,k| h[k] = [] end
+    @cached_addresses = Hash.new do |h,k| h[k] = [] end
   end
 
   def stop
@@ -42,23 +42,53 @@ class Electrum
   end
 
   def sync(store)
+    addresses.each do |address,transactions|
+      store.put(address, transactions)
+    end
+  end
+
+  private
+
+  def update_transaction_cache
+    # electrum holds data in a what that is not optimal for our purposes
+    # each request has a unique address but there is no way to find the balance
+    # on a specific address/request
+    # to get the information we need (each addresses balance and transactions)
+    # first we must list all the transactions (which concert our addresses)
     transactions = query('history')
 
+    # because we're listing every transaction, some of these transactions will
+    # have been already cached and in the store.
+
+    # but it's also possible for an existing transaction to have an updated
+    # number of confirmations, meaning we'll have to update the cached transaction
     transactions.select do |t|
+      # make sure we're only check transactions that have already been cached
       !@cached_transactions[t['txid']].nil?
     end.each do |t|
       cached = @cached_transactions[t['txid']]
 
+      # if the transaction has a different number of confirmations, update
+      # that number and mark it as dirty
       if cached[:confirmations] != t['confirmations']
         cached[:confirmations] = t['confirmations']
         cached[:dirty] = true
       end
     end
 
+    # now, with all the uncached transactions, format them correctly and put
+    # them in the cache as dirty
     transactions.select do |t|
+      # make sure it's not cached
       @cached_transactions[t['txid']].nil?
     end.each do |t|
-      transaction_details = query('deserialize', query('gettransaction', t['txid'])['hex'])
+      # the electrum api is  kinda annoying, in order to get the
+      # recipients and balance of a transaction we have to query:
+      # transaction txid -> serialized transaction data -> transaction info
+      transaction_details = query(
+        'deserialize',
+        query('gettransaction', t['txid'])['hex'])
+
       outputs = transaction_details['outputs'].map do |t_data|
         {
           amount: to_btc(t_data['value']),
@@ -73,25 +103,35 @@ class Electrum
       }
     end
 
+  end
+
+  def update_address_cache
+    update_transaction_cache
+
     @cached_transactions.values.select do |transaction|
       transaction[:dirty]
     end.each do |transaction|
       transaction[:dirty] = false
 
       transaction[:to].each do |to|
-        @cached_address_transactions[to[:address]].push({
+        @cached_addresses[to[:address]].push({
           confirmations: transaction[:confirmations],
           amount: to[:amount]
         })
       end
     end
 
-    @cached_address_transactions.each do |address,transactions|
-      store.put(address, transactions)
-    end
+    query('listrequests')
+      .map do |req| req['address'] end
+      .each do |addr| @cached_addresses[addr] end
   end
 
-  private
+  def addresses
+    update_address_cache
+
+    @cached_addresses
+  end
+
   def to_btc(amount)
     (BigDecimal.new(amount.to_s) / SatoshiPerBitcoin).to_s('f')
   end
