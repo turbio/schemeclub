b1bc235a6893e60edc4323d90d5d5cc339b42186
turbio <masongclayton@gmail.com>
2016-12-25 03:35:30 -0800

    remove mock bitcoin server from bitcoin adapter

--- bitcoin/spec/server_spec.rb
+++ bitcoin/spec/server_spec.rb
@@ -4,8 +4,6 @@ require_relative '../server'
 describe 'server' do
   include SinatraTest
 
-  before do set_driver 'mock' end
-
   it 'should require authentication for all routes' do
     get '/info'
 
@@ -27,60 +25,6 @@ describe 'server' do
     get '/info'
 
     expect(last_response).to be_ok
-    expect(last_response.body).to include '"driver":"mock"'
     expect(last_response.content_type).to eq 'application/json'
-  end
-
-  it 'should create a new address from POSTing to /new' do
-    basic_auth 'user', 'password'
-    post '/new'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to match /"\w{25,35}"/
-    expect(last_response.content_type).to eq 'application/json'
-  end
-
-  it 'should GET a list of all addresses from /all' do
-    basic_auth 'user', 'password'
-    get '/all'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to match /\[.*\]/
-    expect(last_response.content_type).to eq 'application/json'
-
-    original_length = last_response.body.length
-
-    post '/new'
-    new_address = last_response.body
-
-    get '/all'
-
-    expect(original_length).to be < last_response.body.length
-    expect(last_response.body).to include new_address
-  end
-
-  it 'should GET information about address from /:address' do
-    basic_auth 'user', 'password'
-    post '/new'
-    new_address = last_response.body[1...-1]
-
-    get "/#{new_address}"
-
-    expect(last_response).to be_ok
-    expect(last_response.content_type).to eq 'application/json'
-    expect(last_response.body).to match /\[.*\]/
-    expect(last_response.body).to include 'transactions'
-    expect(last_response.body).to include 'balance'
-    expect(last_response.body).to include 'confirmations'
-  end
-
-  it 'should always GET information from /:address' do
-    basic_auth 'user', 'password'
-    get '/not_a_real_address'
-
-    expect(last_response).to be_ok
-    expect(last_response.content_type).to eq 'application/json'
-    expect(last_response.body).to eq(
-      '{"transactions":[],"balance":"0.0","confirmations":0}')
   end
 end
